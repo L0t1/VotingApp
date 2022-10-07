ba19ff86edb3b45114230ced858f292b3d90824b
Lot Kyqyku <lotkyqyku@gmail.com>
2022-10-07 16:18:39 +0200

    .

---
+++ FriendlyVote/tests/Feature/ShowIdeasTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Idea;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShowIdeasTest extends TestCase
+{
+    use RefreshDatabase;
+  
+
+    public function list_of_ideas_shows_on_main_page()
+    {
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My Second Idea',
+            'description' => 'Description of my second idea',
+        ]);
+
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee($ideaOne->title);
+    }
+}
