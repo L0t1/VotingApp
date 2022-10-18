e160fb688e2283527ee564ac8aab55f9edc4edc6
Lot Kyqyku <lotkyqyku@gmail.com>
2022-10-18 10:55:22 +0200

    .

--- FriendlyVote/app/Http/Livewire/IdeasIndex.php
+++ FriendlyVote/app/Http/Livewire/IdeasIndex.php
@@ -16,11 +16,13 @@ class IdeasIndex extends Component
     public $status;
     public $category;
     public $filter;
-
+    public $search;
+    
     protected $queryString = [
         'status',
         'category',
         'filter',
+        'search'
     ];
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -31,6 +33,22 @@ class IdeasIndex extends Component
 
     }
 
+    public function updatingCategory()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+
     public function updatedFilter()
     {
         if($this->filter === 'My Ideas'){
@@ -40,12 +58,6 @@ class IdeasIndex extends Component
         }
     }
   
-
-    public function updatingCategory()
-    {
-        $this->resetPage();
-    }
-
     public function queryStringUpdatedStatus($newStatus)
     {   
         $this->resetPage();
@@ -69,6 +81,8 @@ class IdeasIndex extends Component
                 return $query->orderBy('votes_count');
             })->when($this->filter && $this->filter === 'My Ideas', function ($query) {
                 return $query->where('user_id', auth()->id());
+            })->when(strlen($this->search) >= 3,function ($query) {
+                return $query->where('title', 'like', '%'.$this->search. '%');
             })
             ->addSelect(['voted_by_user' => Vote::select('id')
                     ->where('user_id', auth()->id())
