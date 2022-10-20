a0b5ada8dc770a85238346c0cf82b48c70067900
Lot Kyqyku <lotkyqyku@gmail.com>
2022-10-20 14:06:50 +0200

    .

--- FriendlyVote/resources/views/layouts/app.blade.php
+++ FriendlyVote/resources/views/layouts/app.blade.php
@@ -102,6 +102,12 @@
                 </div>
             </div>
         </main>
+        @if (session('success_message'))
+            <x-notification-success
+                :redirect="true"
+                message-to-display="{{ (session('success_message')) }}"
+            />
+        @endif
+        <livewire:scripts />
     </body>
-    <livewire:scripts />
 </html>
