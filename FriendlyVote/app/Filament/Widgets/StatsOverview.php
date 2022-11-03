f9c3bd6ed04b8daa9c881d56f4d5f5834fd621ba
Lot Kyqyku <lotkyqyku@gmail.com>
2022-11-03 09:52:55 +0100

    .

--- FriendlyVote/app/Filament/Widgets/StatsOverview.php
+++ FriendlyVote/app/Filament/Widgets/StatsOverview.php
@@ -10,16 +10,19 @@ class StatsOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make('User count', '22')
+            Card::make('User count', '21')
             ->description('3%  increase')
             ->descriptionIcon('heroicon-s-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
             ->color('success'),
-        Card::make('Total ideas', '101')
+        Card::make('Total ideas', '98')
             ->description('7% decrease')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
             ->descriptionIcon('heroicon-s-trending-down')
             ->color('danger'),
         Card::make('Average time on page', '3:12')
             ->description('3% increase')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
             ->descriptionIcon('heroicon-s-trending-up')
             ->color('success')
             
